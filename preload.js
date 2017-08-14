c8efa3440799a83573fd03963b06eaeaf6fb6eea
Pollack <apollack@LAMU02T7ACYGTFM.uhc.com>
2017-08-14 14:49:33 -0400

    Altered scrolling continuity

---
+++ preload.js
@@ -0,0 +1,27 @@
+var imgSrcs = ["images/mall-angle-o.jpg",
+"images/organic-tiles.png",
+"images/aerial-o.jpg",
+"images/organic-tiles.png",
+"images/testudo-o.jpg",
+"images/telaviv.jpg"];   // array of URLs
+
+var ids = ["about",
+"about2",
+"team",
+"portfolio",
+"today",
+"contact"];
+
+// var imgs = [];
+//
+// for(var i = 0; i < 6; i++){
+// 	imgs[i] = new Image();
+// 	imgs[i].src = imgSrcs[i];
+// 	// document.getElementById(ids[i]).style.backgroundImage = "url(" + imgSrcs[i] + ")";
+// }
+
+// for(var i = 0; i < 6; i++){
+// 	var img
+// 	var imgData = $('#image').attr('src');
+// 	$(ids[i]).css({'background-image':'url(' + imgData + ')'});
+// }
